c9fbf17ba0c624db3c46c51ef017eee5f635a226
caxap0 <gasbi4ok@gmail.com>
2024-09-23 10:25:01 +0400

    lb2 ready

--- Lab2/client.py
+++ Lab2/client.py
@@ -1,42 +1,50 @@
 import socket
-from Lab1.test import Coder
+from teleseti.Lab1.test import Coder
 import tkinter as tk
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
-def send():
-    message = input_text.get("1.0", tk.END).strip()
-    client_socket.sendto(message.encode('utf-8'), ("127.0.0.1", 12345))
+def gui():
+    root = tk.Tk()
+    main_frame = tk.Frame(root)
+    main_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
 
-# code_message = Coder(message)
-    # win_message = code_message.utf8_to_windows1251()
-    # bin_message = code_message.windows1251_to_bin(win_message)
-    # client_socket.sendto(win_message, ("127.0.0.1", 12345))
-    # client_socket.sendto(bin_message.encode(), ("127.0.0.1", 12345))
+    main_frame.grid_rowconfigure(0, weight=1)
+    main_frame.grid_rowconfigure(1, weight=0)
+    main_frame.grid_columnconfigure(0, weight=1)
+    main_frame.grid_columnconfigure(1, weight=1)
+    main_frame.grid_columnconfigure(2, weight=0)
 
-root = tk.Tk()
-main_frame = tk.Frame(root)
-main_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+    output_text1 = tk.Text(main_frame, height=20, borderwidth=2)
+    output_text1.grid(row=0, column=0, padx=10, pady=5, sticky='nsew')
 
-main_frame.grid_rowconfigure(0, weight=1)
-main_frame.grid_columnconfigure(0, weight=1)  # Месседжбокс 1
-main_frame.grid_columnconfigure(1, weight=0)  # Рамка для кнопок
-main_frame.grid_columnconfigure(2, weight=1)  # Месседжбокс 2
+    output_text2 = tk.Text(main_frame, height=20, borderwidth=2)
+    output_text2.grid(row=0, column=1, padx=10, pady=5, sticky='nsew')
 
-input_text = tk.Text(main_frame, height=20, width=40)
-input_text.grid(row=0, column=0, padx=10, pady=5, sticky='nsew')
+    input_text = tk.Text(main_frame, height=3, borderwidth=2)
+    input_text.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky='nsew')
 
-button_frame = tk.Frame(main_frame)
-button_frame.grid(row=0, column=1, padx=10, pady=5)
+    def send():
+        message = input_text.get("1.0", tk.END).strip()
+        code_message = Coder(message)
+        win_message = code_message.utf8_to_windows1251()
+        hex_encode = ' '.join(f'{byte:02x}' for byte in win_message)
+        bin_message = code_message.windows1251_to_bin(win_message)
+        output_text1.delete("1.0", tk.END)
+        output_text2.delete("1.0", tk.END)
+        output_text1.insert("1.0", f'Вы отправили сообщение: {message}')
+        output_text2.insert("1.0", hex_encode + '\n')
+        output_text2.insert("2.0", bin_message)
 
-output_text = tk.Text(main_frame, height=20, width=40)
-output_text.grid(row=0, column=2, padx=10, pady=5, sticky='nsew')
+        client_message = f'{message}\n{hex_encode}\n{bin_message}'
+        client_socket.sendto(client_message.encode('utf-8'), ("127.0.0.1", 12345))
 
-encode_button = tk.Button(button_frame, text="Отправить", width=20, command=send)
-encode_button.pack(pady=5)
+    send_button = tk.Button(main_frame, text="Отправить", width=20, command=send)
+    send_button.grid(row=1, column=2, padx=10, pady=5, sticky='nsew')
 
-button_frame.grid_rowconfigure(0, weight=1)
-button_frame.grid_columnconfigure(0, weight=1)
+    root.mainloop()
 
-root.mainloop()
+
+if __name__ == "__main__":
+    gui()
--- Lab2/server.py
+++ Lab2/server.py
@@ -1,12 +1,37 @@
 import socket
+import tkinter as tk
+import threading
 
-# Настройка сервера (получателя)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(("127.0.0.1", 12345))
 
-print("Сервер ожидает сообщения...")
 
-# Ожидание сообщения
-while True:
-    message, address = server_socket.recvfrom(1024)  # 1024 - размер буфера
-    print(f"Сообщение от {address}: {message.decode()}")
+def message():
+    while True:
+        output_text.delete(tk.END)
+        message, address = server_socket.recvfrom(1024)
+        output_text.insert("1.0", f'Вам пришло сообщение от {address}: \n{message.decode()}')
+        with open('save.txt', 'a') as file:
+            file.write(f"Сообщение от {address}: {message.decode()}\n")
+
+def treads():
+    tread = threading.Thread(target=message)
+    tread.start()
+
+
+root = tk.Tk()
+main_frame = tk.Frame(root)
+main_frame.pack(padx=10, pady=10, fill=tk.BOTH, expand=True)
+
+main_frame.grid_rowconfigure(0, weight=1)
+main_frame.grid_columnconfigure(0, weight=1)
+
+output_text = tk.Text(main_frame, height=20, borderwidth=2)
+output_text.grid(row=0, column=0, padx=10, pady=5, sticky='nsew')
+
+treads()
+
+root.mainloop()
+
+
+
