bf07876164b9ddf8e98f6f5a5033a1fe9fd2d74a
caxap0 <gasbi4ok@gmail.com>
2024-10-21 10:07:45 +0400

    r

---
+++ Lab3/tcp_server.py
@@ -0,0 +1,24 @@
+from socket import *
+
+tcp_socket = socket(AF_INET, SOCK_STREAM)
+
+tcp_socket.bind(('127.0.0.1', 12345))
+tcp_socket.listen(1)
+
+while True:
+    print('wait connection...')
+
+    conn, addr = tcp_socket.accept()
+    print('client_addr:', addr)
+
+    data = conn.recv(1024)
+    
+    if not data:
+        conn.close()
+        break
+    else:
+        print(data)
+        conn.send(b'Hello from server!')
+        conn.close()
+    
+tcp_socket.close()
